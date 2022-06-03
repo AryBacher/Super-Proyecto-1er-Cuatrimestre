2c4e85413422b424d7642a9aad0c0487b8727fe2
AryBacher <47170979@est.ort.edu.ar>
2022-06-03 09:05:23 -0300

    Initial commit

---
+++ Assets/MovimientoPersonaje.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    bool HasJump;
+    Rigidbody rb;
+    float jumpForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HasJump = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Vertical"));
+
+        //transform.Translate(0, 0, 0.1f * Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 3, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -3, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && HasJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            HasJump = false;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+       if (col.gameObject.name == "DeathWall")
+        {
+            transform.position = new Vector3(4.5f, 0.5f, -13.5f);
+            transform.eulerAngles = new Vector3(0,0,0);
+        }
+
+       if (col.gameObject.name == "Water")
+        {
+            transform.position = new Vector3(4.5f, 0.5f, -13.5f);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (col.gameObject.name == "Piso")
+        {
+                HasJump = true;
+        }
+    }
+}
---
+++ Assets/MovimientoPersonaje2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovimientoPersonaje2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 3, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -3, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "DeathWall")
+        {
+            transform.position = new Vector3(0, 0.5f, -13.5f);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+}
